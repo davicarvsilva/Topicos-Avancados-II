441a2853a2a04827e591b51c95f56abf7c660a33
davicarvsilva <davicarv13@gmail.com>
2023-06-05 17:37:20 -0300

    initial commit

---
+++ atividade-1/Livraria.py
@@ -0,0 +1,82 @@
+"""
+Uma livraria contém 7 títulos distintos e possui um esquema de 
+descontos que leva em consideração se é o mesmo título ou não. 
+O problema consiste em calcular o melhor desconto para o cliente.
+
+Preço de um título qualquer: R$ 42,00
+
+Descontos:
+    2 livros - 5%
+    3 livros - 10%
+    4 livros - 15%
+    5 livros - 20%
+"""
+
+class Livraria:
+    def __init__(self):
+        self.items = [
+            {"id": 1, "titulo": "Harry Potter e a Pedra Filosofal", "preco_unitario": 18.59},
+            {"id": 2, "titulo": "Harry Potter e a Câmara Secreta", "preco_unitario": 15.29},
+            {"id": 3, "titulo": "Harry Potter e o Prisioneiro de Azkaban", "preco_unitario": 16.39},
+            {"id": 4, "titulo": "Harry Potter e o Cálice de Fogo", "preco_unitario": 19.59},
+            {"id": 5, "titulo": "Harry Potter e a Ordem da Fênix", "preco_unitario": 20.29},
+            {"id": 6, "titulo": "Harry Potter e o Enigma do Príncipe", "preco_unitario": 22.89},
+            {"id": 7, "titulo": "Harry Potter e as Relíquias da Morte", "preco_unitario": 27.99},
+        ]
+
+        self.carrinho = []
+
+    def get_item_by_id(self, id):
+        item = next((item for item in self.items if item["id"] == id), None)
+        
+        if item == None:
+            raise Exception("Livro não encontrado. ID incorreto")
+            return {}
+        else:
+            return item
+
+    # adiciona item ao carrinho
+    def adicionar_item_carrinho(self, item):
+        if item:
+            self.carrinho.append(item)
+        
+        else:
+            raise TypeError("Item não informado corretamente")
+
+    # calcula valor total do carrinho, considerando descontos
+    def calcular_valor_carrinho(self):
+        valor_total = 0
+        descontos = self.calcular_descontos_carrinho()
+        for item in self.carrinho:
+            valor_total += item['preco_unitario'] * item['quantidade'] - descontos[item['id']]
+
+        return valor_total
+
+    # calcula desconto de um único item do carrinho
+    def calculcar_desconto_item(self, item):
+        if self.get_item_by_id(item['id']):
+            if item['quantidade'] == 2:
+                percentual =  0.05
+            elif item['quantidade'] == 3:
+                percentual = 0.1 
+            elif item['quantidade'] == 4:
+                percentual = 0.15  
+            else:
+                percentual = 0.2  
+
+            total = item['preco_unitario'] * item['quantidade']
+
+            return total * percentual
+        else:
+            raise TypeError("Item não informado corretamente")
+
+    # calcula desconto de todos os itens do carrinho, e coloca os descontos separados em um dicionario
+    def calcular_descontos_carrinho(self):
+        descontos = {}
+        for item in self.carrinho:
+            descontos[item['id']] = self.calculcar_desconto_item(item)
+        
+        return descontos
+
+    def get_carrinho(self):
+        return self.carrinho
---
+++ atividade-1/tests.py
@@ -0,0 +1,98 @@
+import unittest
+
+from Livraria import Livraria
+
+class TestLivraria(unittest.TestCase):
+    def test_get_item_by_id(self):
+        livraria = Livraria()
+        item = livraria.get_item_by_id(7)
+        
+        self.assertIsInstance(item, dict) # verificar se o item recebido é mesmo um dicionário
+        self.assertNotEqual(item, {}) # verifica se o item recebido não está vazio
+
+    # verificar se item adicionado ao carrinho realmente consta no carrinho
+    def test_adicionar_item_carrinho(self):
+        livraria = Livraria()
+        item = livraria.get_item_by_id(7)
+        item['quantidade'] = 2 # define a quantida de livros a ser comprada
+        livraria.adicionar_item_carrinho(item)
+        expected_item = livraria.get_item_by_id(7)
+        self.assertIn(expected_item, livraria.carrinho) 
+
+    # verifica se o desconto calculado para um item bate com sua respectiva porcentagem
+    # a depender da quantidade de livros 
+    def test_calcular_desconto_item(self):
+        livraria = Livraria()
+
+        item1 = livraria.get_item_by_id(1)
+        item2 = livraria.get_item_by_id(2)
+        item3 = livraria.get_item_by_id(3)
+        item4 = livraria.get_item_by_id(4)
+
+        item1['quantidade'] = 2
+        item2['quantidade'] = 3
+        item3['quantidade'] = 4
+        item4['quantidade'] = 5
+
+        desconto_item1 = item1['quantidade'] * item1['preco_unitario'] * 0.05
+        desconto_item2 = item2['quantidade'] * item2['preco_unitario'] * 0.1
+        desconto_item3 = item3['quantidade'] * item3['preco_unitario'] * 0.15
+        desconto_item4 = item4['quantidade'] * item4['preco_unitario'] * 0.20
+        
+        self.assertEqual(desconto_item1, 
+            livraria.calculcar_desconto_item(item1))
+
+        self.assertEqual(desconto_item2, 
+            livraria.calculcar_desconto_item(item2))
+
+        self.assertEqual(desconto_item3, 
+            livraria.calculcar_desconto_item(item3))
+
+        self.assertEqual(desconto_item4, 
+            livraria.calculcar_desconto_item(item4))
+
+    # verifica se o valor total do carrinho correspondo ao valor total dos itens
+    def test_calcular_valor_carrinho(self):
+        livraria = Livraria()
+        item1 = livraria.get_item_by_id(1)
+        item2 = livraria.get_item_by_id(2)
+
+        item1['quantidade'] = 2
+        item2['quantidade'] = 3
+
+        livraria.adicionar_item_carrinho(item1)
+        livraria.adicionar_item_carrinho(item2)
+
+        desconto_item1 = livraria.calculcar_desconto_item(item1)
+        desconto_item2 = livraria.calculcar_desconto_item(item2)
+
+        desconto_total = desconto_item1 + desconto_item2
+
+        valor_carrinho = livraria.calcular_valor_carrinho()
+
+        self.assertEqual(valor_carrinho, 
+            (item1['preco_unitario'] * item1['quantidade']) + 
+            (item2['preco_unitario'] * item2['quantidade']) - 
+            desconto_total)
+
+    def test_calcular_descontos_carrinho(self):
+        livraria = Livraria()
+        item1 = livraria.get_item_by_id(1)
+        item2 = livraria.get_item_by_id(2)
+
+        item1['quantidade'] = 2
+        item2['quantidade'] = 3
+
+        livraria.adicionar_item_carrinho(item1)
+        livraria.adicionar_item_carrinho(item2)
+
+        desconto_item1 = livraria.calculcar_desconto_item(item1)
+        desconto_item2 = livraria.calculcar_desconto_item(item2)
+
+        descontos = livraria.calcular_descontos_carrinho()
+
+        self.assertEqual(desconto_item1, descontos[item1['id']])
+        self.assertEqual(desconto_item2, descontos[item2['id']])
+
+if __name__ == '__main__':
+    unittest.main()
